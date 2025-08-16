4d471aaddcc6e1f1072f5ceb083398bcbb71c248
Ellizeu Sena <ellizeurs@gmail.com>
2025-08-16 19:00:05 -0300

    Create tests

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 16);
+Smartphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Angry Birds");
